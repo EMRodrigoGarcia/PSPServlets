5e20b99b3b29a03463e374c812f15ef2f7806591
Rodrigo G <rglopez300@gmail.com>
2020-12-17 19:49:54 +0100

    added suma.java (and synced hola.java)

--- apache-tomcat-9.0.41/webapps/primer-servlet/WEB-INF/src/ejemplo/Suma.java
+++ apache-tomcat-9.0.41/webapps/primer-servlet/WEB-INF/src/ejemplo/Suma.java
@@ -4,13 +4,14 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Date;
 
+import javax.print.event.PrintJobListener;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-public class Hola extends HttpServlet {
+public class Suma extends HttpServlet {
     static int visitas = 0;
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
@@ -18,10 +19,9 @@ public class Hola extends HttpServlet {
         PrintWriter out = resp.getWriter();
         out.println("<html>");
         out.println("<body>");
-        out.println("<h1>Hola mundo</h1>");
-        out.println("hoy es " + new Date() + "<br/>");
-        out.println("Visitas: " + incrementar() + "<br/>");
-        out.println("Id del hilo: " + Thread.currentThread().getId());
+        int n1 = Integer.parseInt(req.getParameter("N1"));
+        int n2 = Integer.parseInt(req.getParameter("N2"));
+        out.println("Result: " + (n1 + n2)  + "<br/>");
         out.println("</body>");
         out.println("</html>");
     }
