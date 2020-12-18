9f53f3fbb5ec6c83d78874fe98cf8f69c597652c
Rodrigo García López <rglopez300@gmail.com>
2020-12-18 20:20:33 +0100

    added index.html

--- apache-tomcat-9.0.41/webapps/practica-blog/WEB-INF/src/ejemplo/PlantillasHTML.java
+++ apache-tomcat-9.0.41/webapps/practica-blog/WEB-INF/src/ejemplo/PlantillasHTML.java
@@ -1,5 +1,48 @@
 package ejemplo;
 
 public class PlantillasHTML {
-    // TODO: Insertar plantillas 
+    private static final String mHead = """ 
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Mi Blog</title>
+        <link rel=\"stylesheet" href="./css/bootstrap.css">
+    </head>
+    
+    """;
+    private static final String mCabecera = """
+       <h1 id=\"cabecera\">
+       Blog 
+       </h1>
+    """;
+
+    private static final String mCarta = """
+    <div class=\"card align-self-center\" style=\"width: 30rem;\">
+        <div class=\"card-body\">
+            <h5 class=\"card-title bg-primary text-white\">$titulo$</h5>
+            <p>$fecha$</p>
+            <p class=\"card-text\">$texto$</p>
+        </div>
+    </div>    
+    """;
+
+    private static final String mContainerStart = """
+        <div class=\"container\">
+        <div class=\"col\">
+
+    """;
+    private static final String mContainerEnd = """
+        </div>
+        </div>
+    """;
+    private static final String mFooter = """
+    </body>
+    </html>
+    """;
+
+
+
+
 }
